8eaca18b1fc5d6456f835418aa8857c28b29666f
Haitham Naksho <hnaksho@oakland.edu>
2024-01-16 22:33:19 -0500

    add SixteenLesson.java;

---
+++ codetest/SixteenLesson.java
@@ -0,0 +1,9 @@
+package codetest;
+
+public class SixteenLesson {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
