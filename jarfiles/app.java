515545dfe4764162875a43d07f0fd598ec285076
Haitham Naksho <hnaksho@oakland.edu>
2024-01-18 13:50:34 -0500

    Add existing file

---
+++ jarfiles/app.java
@@ -0,0 +1,12 @@
+package jarfiles;
+//import java.util.Arrays;
+
+public class app {
+    public static void main(String[] args) throws Exception {
+        //System.out.println(args.length);
+        //System.out.println(Arrays.toString(args));
+        double i = Double.parseDouble("123");
+        System.out.println(i);
+        
+    }
+}
